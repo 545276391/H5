35b0bf06bed55aed409f85b2cc310fbffc81b2f8
DESKTOP-UU3MDUP\ruiya <ruiyao_yin@163.com>
2017-04-08 18:25:39 +0800

    add pics

--- waterfall/js/script.js
+++ waterfall/js/script.js
@@ -1,32 +1,107 @@
 window.onload = function() {
-    
-    waterfall();
 
-    let dataInt={'data':[{'src':'0.jpg'},{'src':'1.jpg'},{'src':'2.jpg'},{'src':'3.jpg'}]};
-    	
-    window.onscroll=function(){
-        if(checkScrollSide()){
-        	let main = document.getElementById('main');
-            for(let i=0;i<dataInt.data.length;i++){
-                let oBox=document.createElement('div'); 
-                oBox.className='box';                   
-                main.appendChild(oBox);            
-                let oPic=document.createElement('div');
-                oPic.className='pic';
-                oBox.appendChild(oPic);
-                let oImg=document.createElement('img');
-                oImg.src='./images/'+dataInt.data[i].src;
-                oPic.appendChild(oImg);
-            }
-            waterfall();
-        };
+waterfall();
+
+let dataInt = {
+    'data': [{
+        'src': '0.jpg'
+    }, {
+        'src': '1.jpg'
+    },
+        {
+            'src': '2.jpg'
+        }, {
+            'src': '3.jpg'
+        }, {
+            'src': '4.jpg'
+        },
+        {
+            'src': '5.jpg'
+        }, {
+            'src': '6.jpg'
+        }, {
+            'src': '7.jpg'
+        },
+        {
+            'src': '8.jpg'
+        }, {
+            'src': '9.jpg'
+        }, {
+            'src': '10.jpg'
+        },
+        {
+            'src': '11.jpg'
+        }, {
+            'src': '12.jpg'
+        }, {
+            'src': '13.jpg'
+        },
+        {
+            'src': '14.jpg'
+        }, {
+            'src': '15.jpg'
+        }, {
+            'src': '16.jpg'
+        },
+        {
+            'src': '17.jpg'
+        }, {
+            'src': '18.jpg'
+        }, {
+            'src': '19.jpg'
+        },
+        {
+            'src': '20.jpg'
+        }, {
+            'src': '21.jpg'
+        }, {
+            'src': '22.jpg'
+        },
+        {
+            'src': '23.jpg'
+        }, {
+            'src': '24.jpg'
+        }, {
+            'src': '25.jpg'
+        },
+        {
+            'src': '26.jpg'
+        }, {
+            'src': '27.jpg'
+        }, {
+            'src': '28.jpg'
+        },
+        {
+            'src': '29.jpg'
+        }, {
+            'src': '30.jpg'
+        }]
+};
+
+window.onscroll = function() {
+    if (checkScrollSide()) {
+        let main = document.getElementById('main');
+        for (let i = 0; i < dataInt.data.length; i++) {
+            let oBox = document.createElement('div');
+            oBox.className = 'box';
+            main.appendChild(oBox);
+            let oPic = document.createElement('div');
+            oPic.className = 'pic';
+            oBox.appendChild(oPic);
+            let oImg = document.createElement('img');
+            oImg.src = './images/' + dataInt.data[i].src;
+            oPic.appendChild(oImg);
+        }
+        waterfall();
     }
+    ;
+}
 }
 
 
 
 function waterfall() {
-	let main = document.getElementById('main');
+    let main = document.getElementById('main');
     let boxArr = document.getElementsByClassName('box');
     let oBoxW = boxArr[0].offsetWidth;
     let cols = Math.floor(document.documentElement.clientWidth / oBoxW);
@@ -41,7 +116,7 @@ function waterfall() {
             let index = getMinhIndex(hArr, minH);
             boxArr[i].style.position = 'absolute';
             boxArr[i].style.top = minH + 'px';
-            boxArr[i].style.left = boxArr[index].offsetLeft+'px';
+            boxArr[i].style.left = boxArr[index].offsetLeft + 'px';
             hArr[index] += boxArr[i].offsetHeight;
         }
     }
@@ -55,12 +130,10 @@ function getMinhIndex(arr, val) {
     }
 }
 
-function checkScrollSide(){
+function checkScrollSide() {
     let boxArr = document.getElementsByClassName('box');
-	let lastBoxH = boxArr[boxArr.length-1].offsetTop + Math.floor(boxArr[boxArr.length-1].offsetHeight/2);
-	let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-	let height = document.body.clientHeight || document.documentElement.clientHeight;
-	return (lastBoxH<scrollTop+height) ? true:false;
+    let lastBoxH = boxArr[boxArr.length - 1].offsetTop + Math.floor(boxArr[boxArr.length - 1].offsetHeight / 2);
+    let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.body.clientHeight || document.documentElement.clientHeight;
+    return (lastBoxH < scrollTop + height) ? true : false;
 }
-
-
--- waterfall/js/script_jq.js
+++ waterfall/js/script_jq.js
@@ -1,48 +1,122 @@
-$(window).on('load',function(){
+$(window).on('load', function() {
 
-	waterfall();
+    waterfall();
 
-	let dataInt={'data':[{'src':'0.jpg'},{'src':'1.jpg'},{'src':'2.jpg'},{'src':'3.jpg'}]};
-	
-	$(window).on('scroll',function(){
-		if(checkScrollSide()){
-			$.each(dataInt.data,function(key,value){
-				let oBox = $('<div>').addClass('box').appendTo($('#main'));
-				let oPic = $('<div>').addClass('pic').appendTo($(oBox));
-				$('<img>').attr('src','images/'+ $(value).attr('src')).appendTo($(oPic));
-			});
-			waterfall();
-		}
-	});
+    let dataInt = {
+        'data': [{
+            'src': '0.jpg'
+        }, {
+            'src': '1.jpg'
+        },
+            {
+                'src': '2.jpg'
+            }, {
+                'src': '3.jpg'
+            }, {
+                'src': '4.jpg'
+            },
+            {
+                'src': '5.jpg'
+            }, {
+                'src': '6.jpg'
+            }, {
+                'src': '7.jpg'
+            },
+            {
+                'src': '8.jpg'
+            }, {
+                'src': '9.jpg'
+            }, {
+                'src': '10.jpg'
+            },
+            {
+                'src': '11.jpg'
+            }, {
+                'src': '12.jpg'
+            }, {
+                'src': '13.jpg'
+            },
+            {
+                'src': '14.jpg'
+            }, {
+                'src': '15.jpg'
+            }, {
+                'src': '16.jpg'
+            },
+            {
+                'src': '17.jpg'
+            }, {
+                'src': '18.jpg'
+            }, {
+                'src': '19.jpg'
+            },
+            {
+                'src': '20.jpg'
+            }, {
+                'src': '21.jpg'
+            }, {
+                'src': '22.jpg'
+            },
+            {
+                'src': '23.jpg'
+            }, {
+                'src': '24.jpg'
+            }, {
+                'src': '25.jpg'
+            },
+            {
+                'src': '26.jpg'
+            }, {
+                'src': '27.jpg'
+            }, {
+                'src': '28.jpg'
+            },
+            {
+                'src': '29.jpg'
+            }, {
+                'src': '30.jpg'
+            }]
+    };
+
+    $(window).on('scroll', function() {
+        if (checkScrollSide()) {
+            $.each(dataInt.data, function(key, value) {
+                let oBox = $('<div>').addClass('box').appendTo($('#main'));
+                let oPic = $('<div>').addClass('pic').appendTo($(oBox));
+                $('<img>').attr('src', 'images/' + $(value).attr('src')).appendTo($(oPic));
+            });
+            waterfall();
+        }
+    });
 });
 
 function waterfall() {
     let $boxArr = $('#main>div');
     let oBoxW = $boxArr.eq(0).outerWidth();
     let cols = Math.floor($(window).width() / oBoxW);
-    $('#main').width(oBoxW * cols).css('margin','0 auto');
+    $('#main').width(oBoxW * cols).css('margin', '0 auto');
 
     let hArr = [];
-    $boxArr.each(function(index,value){
-    	if(index<cols){
-    		hArr.push($boxArr.eq(index).outerHeight());
-    	}else{
-    		let minH = Math.min.apply(null, hArr);
-    		let minHIndex = $.inArray(minH,hArr);
-    		$(value).css({
-    			'position':'absolute',
-    			'top':minH+'px',
-    			'left':minHIndex*oBoxW+'px'
-    		});
-    		hArr[minHIndex]+=$boxArr.eq(index).outerHeight();
-    	}
+    $boxArr.each(function(index, value) {
+        if (index < cols) {
+            hArr.push($boxArr.eq(index).outerHeight());
+        } else {
+            let minH = Math.min.apply(null, hArr);
+            let minHIndex = $.inArray(minH, hArr);
+            $(value).css({
+                'position': 'absolute',
+                'top': minH + 'px',
+                'left': minHIndex * oBoxW + 'px'
+            });
+            hArr[minHIndex] += $boxArr.eq(index).outerHeight();
+        }
     });
 }
 
-function checkScrollSide(){
-	let $lastBox = $('#main>div').last();
-	let lastBoxH = $lastBox.offset().top + Math.floor($lastBox.outerHeight()/2);
-	let scrollTop = $(window).scrollTop();
-	let height = $(window).height();
-	return (lastBoxH<scrollTop+height) ? true:false;
+function checkScrollSide() {
+    let $lastBox = $('#main>div').last();
+    let lastBoxH = $lastBox.offset().top + Math.floor($lastBox.outerHeight() / 2);
+    let scrollTop = $(window).scrollTop();
+    let height = $(window).height();
+    return (lastBoxH < scrollTop + height) ? true : false;
 }
